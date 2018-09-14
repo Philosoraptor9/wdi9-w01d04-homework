db8eb2183f4305e2cafb1a40b0cd54b3d2bf34c0
Philip Hoover <philhoov90@gmail.com>
2018-09-14 08:36:19 -0600

    Commit 1 Verbal Questions

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log('here we go again');
+
+// VERBAL QUESTIONS
+// 1. A parameter is a data type that a function must take in to run, an argument is passed when calling the function
+// 2. Console.log just shows a value on the console, return performs spits out a value that has been operated on by the function
+// 3. A function that returns a value is doing what it's supposed to do, or what javascript is being told to do
+
